83cccbea72c2c692fa707fa37124188d45afc6fd
Alex Prestes <alex.prestes@usp.br>
2020-02-25 17:03:03 -0300

    prog2

--- Projeto 1/prog2.f90
+++ Projeto 1/prog2.f90
@@ -7,9 +7,12 @@ real*8 :: area
 
 print *, 'Digite v1: x1 y1 z1'
 read(*,*) v1
-print *, '\nDigite v2: x2 y2 z2'
+print *, 'Digite v2: x2 y2 z2'
 read(*,*) v2
 
-area = (
+area = (v1(2)*v2(3) - v2(2)*v1(3))**2 + (v2(1)*v1(3) - v1(1)*v1(3))**2 + (v1(1)*v2(2) - v2(1)*v1(2))**2
+area = sqrt(area)/2.d0
+
+print *, area
 
 end program prog2
