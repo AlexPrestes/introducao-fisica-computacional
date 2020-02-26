8805cc4b5582a49f26b8191906d7233c30b869f8
Arex Prestes <alex.prestes@usp.br>
2020-02-26 14:43:07 -0300

    finalizando prog5

--- Projeto 1/prog5.f90
+++ Projeto 1/prog5.f90
@@ -1,25 +1,55 @@
 program prog5
 
     implicit none
-
+    
     integer :: n
-    real*8 :: lnx, x, erro, logx
-    real*8, parameter :: eprec = 10e-5
-
+    real :: lnx, x, erro, logx
+    real, parameter :: eprec = 1e-5
+    
+    real*8 :: dlnx, dx, derro, dlogx, deprec
+    
     erro = 2*eprec
     lnx = 0
     n = 0
-
+    
     print *, 'Digite um x Real entre 0 e 2:'
     read (*,*) x
-
+    
     logx = log(x)
-
+    
     do while (erro >= eprec)
         n = n + 1
         lnx = lnx -(1 - x)**n/n
         erro = abs(logx - lnx)
-        print *, logx, lnx, erro, eprec, n
     end do
-
+    
+    print *, 'Precisão Simples'
+    print *, 'Num iter: ', n
+    print *, 'log(x):   ', logx
+    print *, 'Aprox:    ', lnx
+    print *, 'erro:    ', erro
+    
+    
+    dx = x
+    deprec = epsilon(dx)
+    derro = 2*deprec
+    dlnx = 0
+    n = 0
+    
+    dlogx = dlog(dx)
+    
+    do while (derro > deprec)
+        n = n + 1
+        dlnx = dlnx -(1 - dx)**n/n
+        derro = abs(dlogx - dlnx)
+    end do
+    
+    print *, ''
+    print *, ''
+    print *, 'Precisão Dupla'
+    print *, 'Num iter: ', n
+    print *, 'dlog(x):   ', dlogx
+    print *, 'Aprox:    ', dlnx
+    print *, 'erro:    ', derro
+    
 end program prog5
