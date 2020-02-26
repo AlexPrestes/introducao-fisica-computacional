b4a94275f0cfe9fdfbdff7fc1ab8e767e5724ffe
Arex Prestes <alex.prestes@usp.br>
2020-02-26 14:14:42 -0300

    correção código

--- Projeto 1/prog5.f90
+++ Projeto 1/prog5.f90
@@ -1,23 +1,25 @@
 program prog5
 
-implicit none
+    implicit none
 
-integer :: n
-real*8 :: lnx, x, erro
-real*8, parameter :: eprec = 10e-5
+    integer :: n
+    real*8 :: lnx, x, erro, logx
+    real*8, parameter :: eprec = 10e-5
 
-erro = 2*eprec
-lnx = 0
-n = 1
+    erro = 2*eprec
+    lnx = 0
+    n = 0
 
-print *, 'Digite um x:'
-read (*,*) x
+    print *, 'Digite um x Real entre 0 e 2:'
+    read (*,*) x
 
-do while (erro >= eprec)
-    lnx = lnx + (-(1 - x)**n)/n
-    erro = abs(log(x)-lnx)
-    print *, log(x), lnx, erro, eprec
-    n = n + 1
-end do
+    logx = log(x)
+
+    do while (erro >= eprec)
+        n = n + 1
+        lnx = lnx -(1 - x)**n/n
+        erro = abs(logx - lnx)
+        print *, logx, lnx, erro, eprec, n
+    end do
 
 end program prog5
