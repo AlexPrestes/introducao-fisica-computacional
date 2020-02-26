608782a639a3e140e11f0955b8f3b9f1cf395a33
Alex Prestes <alex.prestes@usp.br>
2020-02-26 03:39:28 -0300

    prog5 - incompleto

---
+++ Projeto 1/prog5.f90
@@ -0,0 +1,23 @@
+program prog5
+
+implicit none
+
+integer :: n
+real*8 :: lnx, x, erro
+real*8, parameter :: eprec = 10e-5
+
+erro = 2*eprec
+lnx = 0
+n = 1
+
+print *, 'Digite um x:'
+read (*,*) x
+
+do while (erro >= eprec)
+    lnx = lnx + ((-1)**(n+1))*(((1 - x)**n)/n)
+    erro = abs(log(x)-lnx)
+    print *, log(x), lnx, erro, eprec
+    n = n + 1
+end do
+
+end program prog5
