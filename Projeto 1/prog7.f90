c4022db932c4839dbd0538c21844cb9c47172d53
Arex Prestes <alex.prestes@usp.br>
2020-03-04 11:18:10 -0300

    prog7 finalizado

--- Projeto 1/prog7.f90
+++ Projeto 1/prog7.f90
@@ -4,7 +4,8 @@ program prog7
     
     integer*8 :: i, j
     integer*8 :: d, M, N
-    real*8 :: rmod, R
+    real*8 :: rmod, R, Vd, dgamma, arg
+    real*8, parameter :: pi = 4d0*atan(1d0)
     real*8, dimension(:), allocatable :: p
     
     print *, 'Digite o número de dimensões d:'
@@ -38,5 +39,26 @@ program prog7
     end do
     print *, 'Finalizado...'
     print *, (dfloat(N)/dfloat(M))*((2d0*R)**d)
+    
+    arg = (d/2d0 + 1d0)
+    dgamma = 1d0
+    
+    arg = arg - 1
+    
+    !print *, sqrt(pi)
+    
+    do while (arg > 0d0)
+        if (arg >= 1d0) then
+            dgamma = arg*dgamma
+            arg = arg - 1d0
+        else
+            dgamma = arg*sqrt(pi)*dgamma
+            arg = 0
+        end if
+    end do
+    
+    Vd = (pi**(d/2) * R**d) / dgamma
+    
+    print *, Vdd
     
 end program prog7
