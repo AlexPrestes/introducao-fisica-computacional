742f83fe34fb4cb357d16de1c639138e7377ece9
Alex Prestes <alex.prestes@usp.br>
2020-03-07 16:42:33 -0300

    adicionando comentários

--- projeto-1/tarefa-3/tarefa-3-10407962.f90
+++ projeto-1/tarefa-3/tarefa-3-10407962.f90
@@ -1,30 +1,40 @@
 program tarefa3
-
+    
+    !definindo um n maximo para o arquivo de entrada
     parameter (nmax=10000)
-    dimension NumList(nmax)
+    !vetor que irá receber os valores do arquivo de entrada
+    dimension ANumList(nmax)
     
-    open (unit=10, file='entrada-3-10407962')
-    open (unit=11, file='saida-3-10407962')
+    !definindo os arquivos
+    open(unit=10, file='entrada-3-10407962')
+    open(unit=11, file='saida-3-10407962')
     
-    read (10,*) N
-    allocate(NumList(N))
-    read (10,*) NumList
-    close(10)
+    !loop para atribuir valores do arquivo para o vetor
+    do n = 1, nmax
+        read(10, *, end=1) ANumList(n)
+    end do
+1   close(10)
+    !como o n final é +1 que o número de valores é necessário corrigir
+    n = n-1
+
+    !recebe o número inteiro M
     print *, 'Digite o valor de M (Inteiro):'
     read (*,*) M
     
-    do i = N, 1, -1
-        do j = N, 2, -1
-            if (NumList(j) < NumList(j-1)) then
-                aux = NumList(j)
-                NumList(j) = NumList(j-1)
-                NumList(j-1) = aux
+    !Algoritmo de ordenação, verifica termo a termo
+    do i = n, 1, -1
+        do j = n, 2, -1
+            if (ANumList(j) < ANumList(j-1)) then
+                aux = ANumList(j)
+                ANumList(j) = ANumList(j-1)
+                ANumList(j-1) = aux
             end if
         end do
     end do
     
+    !gera o arquivo de saida com os M primeiros números
     do i = 1, M
-        write (11,*) NumList(i)
+        write (11,*) ANumList(i)
     end do
     close (11)
 
