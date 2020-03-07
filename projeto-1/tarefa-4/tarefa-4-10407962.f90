b07ac3c3260731fe1c11a84a525108ccc0cc6265
Arex Prestes <alex.prestes@usp.br>
2020-03-07 00:29:19 -0300

    tarefa-4 finalizado

--- projeto-1/tarefa-4/tarefa-4-10407962.f90
+++ projeto-1/tarefa-4/tarefa-4-10407962.f90
@@ -1,30 +1,20 @@
-program prog4
-
-implicit none
-
-integer :: N, i, j, resto
-real, dimension(:), allocatable :: primos
-
-open(unit=11, file='prog4.out')
-
-print *, 'Digite um inteiro N:'
-read (*,*) N
-
-allocate(primos(N-1))
-
-do i = 2, N
-    primos(i-1) = i
-end do
-
-do i = 1, N-1
-    do j = i+1, N-1
-        if (mod(primos(j), primos(i)) == 0) then
-            primos(j) = 0
+program tarefa4
+    
+    open(unit=11, file='saida-4-10407962')
+    
+    print *, 'Digite um inteiro N:'
+    read (*,*) N
+    
+    do i = 2, N
+        k = i
+        do j = 2, i/2
+            if (mod(i, j) == 0) then
+                k = 0
+            end if
+        end do
+        if (k /= 0) then
+            write (11, *) i
         end if
     end do
-    if (primos(i) /= 0) then
-        write (11,*) int(primos(i))
-    end if
-end do
 
-end program prog4
+end program tarefa4
