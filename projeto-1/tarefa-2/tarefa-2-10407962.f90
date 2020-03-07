3fc9dc97c890097f30c97be4ddb5c76833447b91
Arex Prestes <alex.prestes@usp.br>
2020-03-07 15:44:54 -0300

    adicionando comentarios

--- projeto-1/tarefa-2/tarefa-2-10407962.f90
+++ projeto-1/tarefa-2/tarefa-2-10407962.f90
@@ -1,13 +1,15 @@
 program tarefa2
     
+    !Recebe os valores dos vetores
     print *, 'Digite V1: (x1, y1, z1)'
     read(*,*) x1, y1, z1
     print *, 'Digite V2: (x2, y2, z2)'
     read(*,*) x2, y2, z2
     
+    !Calcula o módulo do vetor e divide por 2, para obter a área do triângulo
     area = (y1*z2 - y2*z1)**2 + (x2*z1 - x1*z2)**2 + (x1*y2 - x2*y1)**2
     area = sqrt(area)/2.d0
-    
+    !imprime a área
     print '(A, F8.5)', "Área triângulo: ", area
 
 end program tarefa2
