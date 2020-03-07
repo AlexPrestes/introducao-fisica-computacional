9501b946678ee6a6f9d0a422a7389461ffe1d481
Arex Prestes <alex.prestes@usp.br>
2020-03-06 23:32:22 -0300

    finalizado

--- projeto-1/tarefa-2/tarefa-2-10407962.f90
+++ projeto-1/tarefa-2/tarefa-2-10407962.f90
@@ -1,18 +1,13 @@
-program prog2
+program tarefa2
+    
+    print *, 'Digite V1: (x1, y1, z1)'
+    read(*,*) x1, y1, z1
+    print *, 'Digite V2: (x2, y2, z2)'
+    read(*,*) x2, y2, z2
+    
+    area = (y1*z2 - y2*z1)**2 + (x2*z1 - x1*z2)**2 + (x1*y2 - x2*y1)**2
+    area = sqrt(area)/2.d0
+    
+    print '(A, F8.5)', "Área triângulo: ", area
 
-implicit none
-
-real*8, dimension(3) :: v1, v2
-real*8 :: area
-
-print *, 'Digite v1: x1 y1 z1'
-read(*,*) v1
-print *, 'Digite v2: x2 y2 z2'
-read(*,*) v2
-
-area = (v1(2)*v2(3) - v2(2)*v1(3))**2 + (v2(1)*v1(3) - v1(1)*v1(3))**2 + (v1(1)*v2(2) - v2(1)*v1(2))**2
-area = sqrt(area)/2.d0
-
-print *, area
-
-end program prog2
+end program tarefa2
