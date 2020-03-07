d8d1e1d7813d0fcce66b29a846823f8a3e2f0e33
Alex Prestes <alex.prestes@usp.br>
2020-03-07 17:06:54 -0300

    modificação e comentários

--- projeto-1/tarefa-4/tarefa-4-10407962.f90
+++ projeto-1/tarefa-4/tarefa-4-10407962.f90
@@ -2,18 +2,26 @@ program tarefa4
     
     open(unit=11, file='saida-4-10407962')
     
+    ! Recebe um inteiro N
     print *, 'Digite um inteiro N:'
     read (*,*) N
     
+    ! Loop i indo de 2 até N
     do i = 2, N
+        ! Variável k auxiliar, recebe o valor de i
         k = i
+        ! Loop j procura por 2 até i/2
         do j = 2, i/2
+            ! Verifica se i/j é divisivel
             if (mod(i, j) == 0) then
+                ! caso seja divisivel k = 0,
+                ! para que não seja escrito no arquivo de saida
                 k = 0
             end if
         end do
+        ! condição para escrever no arquivo de saida
         if (k /= 0) then
-            write (11, *) i
+            write (11, '(I0)') i
         end if
     end do
     
