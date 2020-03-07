a7f448bb779e69e1ab35a7320c369ce1c5561114
Arex Prestes <alex.prestes@usp.br>
2020-03-07 01:21:40 -0300

    tarefa-5 finalizada

--- projeto-1/tarefa-5/tarefa-5-10407962.f90
+++ projeto-1/tarefa-5/tarefa-5-10407962.f90
@@ -1,55 +1,48 @@
-program prog5
-
-    implicit none
+program tarefa5
     
-    integer :: n
-    real :: lnx, x, erro, logx
-    real, parameter :: eprec = 1e-5
+    real*8 :: dlnx, dx, derro, dlogx
     
-    real*8 :: dlnx, dx, derro, dlogx, deprec
-    
+    eprec = 10e-5
     erro = 2*eprec
-    lnx = 0
+    slnx = 0e0
     n = 0
     
     print *, 'Digite um x Real entre 0 e 2:'
     read (*,*) x
-    
-    logx = log(x)
+    slogx = log(x)
     
     do while (erro >= eprec)
         n = n + 1
-        lnx = lnx -(1 - x)**n/n
-        erro = abs(logx - lnx)
+        slnx = slnx -(1e0 - x)**n/n
+        erro = abs(slogx - slnx)
     end do
     
     print *, 'Precisão Simples'
     print *, 'Num iter: ', n
-    print *, 'log(x):   ', logx
-    print *, 'Aprox:    ', lnx
+    print *, 'log(x):   ', slogx
+    print *, 'Aprox:    ', slnx
     print *, 'erro:    ', erro
     
     
     dx = x
-    deprec = epsilon(dx)
-    derro = 2*deprec
-    dlnx = 0
+    derro = 0d0
+    dlnx = 0d0
     n = 0
     
     dlogx = dlog(dx)
     
-    do while (derro > deprec)
+    do while (derro /= abs(dlogx - dlnx))
+        derro = abs(dlogx - dlnx)
         n = n + 1
         dlnx = dlnx -(1 - dx)**n/n
-        derro = abs(dlogx - dlnx)
     end do
     
     print *, ''
     print *, ''
     print *, 'Precisão Dupla'
-    print *, 'Num iter: ', n
+    print *, 'Num iter: ', n-1
     print *, 'dlog(x):   ', dlogx
     print *, 'Aprox:    ', dlnx
     print *, 'erro:    ', derro
     
-end program prog5
+end program tarefa5
