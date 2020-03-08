0f3c27e0201022bc6347eb474494efe76a625740
Alex Prestes <alex.prestes@usp.br>
2020-03-07 22:26:45 -0300

    comentários

--- projeto-1/tarefa-6/tarefa-6-10407962.f90
+++ projeto-1/tarefa-6/tarefa-6-10407962.f90
@@ -1,11 +1,18 @@
 program tarefa6
-    
+
+    ! Definindo pi 
     pi = 4*atan(1e0)
     
+    ! Recebe o valor inteiro de N
     print *, 'Raízes da equação (Z - 2)**N = 3'
     print *, 'Digite o valor inteiro de N:'
     read (*,*) N
     
+    ! loop k para solução geral z = |z|**(1/n) * ( cos( (theta+2*pi*k)/n ) + i*sin( (theta+2*pi*k)/n ) )
+    ! forma polar do complexo z
+    ! na equação theta = 0
+    ! arg = 2*pi*k/n
+    ! zmod = |z|
     do k = 1, N
         arg = 2e0*pi*k/N
         zmod = 3e0**(1e0/N)
