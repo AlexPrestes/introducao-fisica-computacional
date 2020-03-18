5077720ec289533a14041191136e62204ba6efc4
Alex Prestes <alex.prestes@usp.br>
2020-03-18 17:48:45 -0300

    tarefa b

--- projeto-2/tarefa-b/tarefa-b-10407962.f90
+++ projeto-2/tarefa-b/tarefa-b-10407962.f90
@@ -1,34 +1,33 @@
 program tarefab
-      parameter (Nmax = 2*1000+1)
-      dimension iposicao(Nmax)
+    
+    parameter (N = 1001)
+    dimension iposicao(-N:N)
 
-      open(10, file='saida-b1')
+    open(10, file='saida-b1')
 
-      print *, "valor p:"
-      read(*,*) p
+    print *, "valor p:"
+    read(*,*) p
 
-      print *, "número de bebados M:"
-      read(*,*) M
+    print *, "número de bebados M:"
+    read(*,*) M
 
-      N = 1000
-      q = 1 - p
-      iposicao = 0
+    q = 1 - p
+    iposicao = 0
 
-      do j = 1, M !quantidade de bebados
-          k = 1
-          do i = 1, N !quatidade de passos
-              if (rand().lt.p) then !direcao do passo
-                  k = k + 1
-              else
-                  k = k - 1
-              end if
-          end do
-          iposicao(k+N) = iposicao(k+N) + 1
-      end do
+    do j = 1, M !quantidade de bebados
+        k = 0
+        do i = 1, N !quatidade de passos
+            if (rand().lt.p) then !direcao do passo
+                k = k + 1
+            else
+                k = k - 1
+            end if
+        end do
+        iposicao(k) = iposicao(k) + 1
+    end do
       
-      do i = -N, N 
-        
-        write(10,'(I0," ",I0)') i, iposicao(i+N+1)
-      end do
+    do i = -N, N
+        write(10,'(I0," ",I0)') i, iposicao(i)
+    end do
 
  end program tarefab
