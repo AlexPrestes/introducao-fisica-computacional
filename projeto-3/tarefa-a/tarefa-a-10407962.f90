08848c09d6bbaa64bb4fe3cc13d337fdff6bfdda
Alex Prestes <alex.prestes@usp.br>
2020-04-11 01:59:20 -0300

    com erro de tuncamento?

--- projeto-3/tarefa-a/tarefa-a-10407962.f90
+++ projeto-3/tarefa-a/tarefa-a-10407962.f90
@@ -1,28 +1,40 @@
 program tarefaa
     implicit real*8 (a-h,o-z)
 
+    dimension h(14)
+
     f(x) = dsinh(2*x)*dsin(x/4)
     df(x) = 2*dcosh(2*x)*dsin(x/4) + dcos(x/4)*dsinh(2*x)/4
+    d2f(x) = dcos(x/4)*dcosh(2*x) + 63*dsin(x/4)*dsinh(2*x)/16
+    d3f(x) = 61*dcosh(2*x)*dsin(x/4)/8 + 191*dcos(x/4)*dsinh(2*x)/64 
 
-    dfrente2p(x, h) = ( f(x+h) - f(x) ) / h
-    dtras2p(x, h) = ( f(x) - f(x-h) ) / h
-    dsimetrica3p(x, h) = ( f(x+h) - f(x-h) ) / ( 2*h )
-    dsimetrica5p(x, h) = ( f(x-2*h) -8*f(x-h) +8*f(x+h) -f(x-2*h) ) / ( 12*h )
+    dfrente2p(x, h_i) = ( f(x+h_i) - f(x) ) / h_i
+    dtras2p(x, h_i) = ( f(x) - f(x-h_i) ) / h_i
+    dsimetrica3p(x, h_i) = ( f(x+h_i) - f(x-h_i) ) / ( 2*h_i )
+    dsimetrica5p(x, h_i) = ( f(x-2*h_i) -8*f(x-h_i) +8*f(x+h_i) -f(x-2*h_i) ) / ( 12*h_i )
+    d2simetrica5p(x, h_i) = ( -f(x-2*h_i) +16*f(x-h_i) -30*f(x) +16*f(x+h_i) -f(x+2*h_i) ) / ( 12*h_i**2 )
+    d3antisimetrica5p(x, h_i) = ( -f(x-2*h_i) +2*f(x-h_i) -2*f(x+h_i) +f(x+2*h_i) ) / ( 2*h_i**3 )
+
+    h = (/0.5, 0.2, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001, 0.000001, 0.0000001, 0.00000001/)
+
+    open(10, file='saida-a-10407962')
 
     write(*,*) 'Escreva o valor de x:'
     read(*,*) x
 
     df_x = df(x)
 
-    do i = 2, 10
-        h = 1d0/2d0**i
+    do i = 1, 14
 
-        dfrente2p_x_h = dfrente2p(x, h)
-        dtras2p_x_h = dtras2p(x, h)
-        dsimetrica3p_x_h = dsimetrica3p(x, h)
-        dsimetrica5p_x_h = dsimetrica5p(x, h)
+        dfrente2p_x_h = dfrente2p(x, h(i))
+        dtras2p_x_h = dtras2p(x, h(i))
+        dsimetrica3p_x_h = dsimetrica3p(x, h(i))
+        dsimetrica5p_x_h = dsimetrica5p(x, h(i))
+        d2simetrica5p_x_h = d2simetrica5p(x, h(i))
+        d3antisimetrica5p_x_h = d3antisimetrica5p(x, h(i))
+
         
-        write(*,*) h, dfrente2p_x_h-df_x, dtras2p_x_h-df_x, dsimetrica3p_x_h-df_x, dsimetrica5p_x_h-df_x
+        write(10,*) h(i), abs(dfrente2p_x_h-df_x), abs(dtras2p_x_h-df_x), abs(dsimetrica3p_x_h-df_x), abs(dsimetrica5p_x_h-df_x), abs(d2simetrica5p_x_h-d2f(x)), abs(d3antisimetrica5p_x_h-d3f(x))
     end do
 
 end program tarefaa
