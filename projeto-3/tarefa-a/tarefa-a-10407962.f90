a0824443127378d03cb5d069d7f8374f875a979c
Alex Prestes <alex.prestes@usp.br>
2020-04-11 00:10:41 -0300

    alterçoes codigo + vscode

--- projeto-3/tarefa-a/tarefa-a-10407962.f90
+++ projeto-3/tarefa-a/tarefa-a-10407962.f90
@@ -1,46 +1,28 @@
 program tarefaa
     implicit real*8 (a-h,o-z)
 
+    f(x) = dsinh(2*x)*dsin(x/4)
+    df(x) = 2*dcosh(2*x)*dsin(x/4) + dcos(x/4)*dsinh(2*x)/4
+
+    dfrente2p(x, h) = ( f(x+h) - f(x) ) / h
+    dtras2p(x, h) = ( f(x) - f(x-h) ) / h
+    dsimetrica3p(x, h) = ( f(x+h) - f(x-h) ) / ( 2*h )
+    dsimetrica5p(x, h) = ( f(x-2*h) -8*f(x-h) +8*f(x+h) -f(x-2*h) ) / ( 12*h )
+
     write(*,*) 'Escreva o valor de x:'
     read(*,*) x
 
+    df_x = df(x)
+
     do i = 2, 10
-        
         h = 1d0/2d0**i
-        write(*,*) h, dfrente2p(f, x, h)-df(x), dtras2p(f, x, h)-df( x), dsimetrica3p(f, x, h)-df(x), dsimetrica5p(f, x, h)-df(x)
 
+        dfrente2p_x_h = dfrente2p(x, h)
+        dtras2p_x_h = dtras2p(x, h)
+        dsimetrica3p_x_h = dsimetrica3p(x, h)
+        dsimetrica5p_x_h = dsimetrica5p(x, h)
+        
+        write(*,*) h, dfrente2p_x_h-df_x, dtras2p_x_h-df_x, dsimetrica3p_x_h-df_x, dsimetrica5p_x_h-df_x
     end do
-
-contains
-
-    function f(x)
-        f = dsinh(2*x)*dsin(x/4)
-        return
-    end function
-
-    function df(x)
-        df = 2*dcosh(2*x)*dsin(x/4) + dcos(x/4)*dsinh(2*x)/4
-        return
-    end function
-
-    function dfrente2p(f, x, h)
-        dfrente2p = ( f(x+h) - f(x) ) / h
-        return
-    end function
-
-    function dtras2p(f, x, h)
-        dtras2p = ( f(x) - f(x-h) ) / h
-        return
-    end function
-
-    function dsimetrica3p(f, x, h)
-        dsimetrica3p = ( f(x+h) - f(x-h) ) / ( 2*h )
-        return
-    end function
-
-    function dsimetrica5p(f, x, h)
-        dsimetrica5p = ( f(x-2*h) -8*f(x-h) +8*f(x+h) -f(x-2*h) ) / ( 12*h )
-        return
-    end function
 
 end program tarefaa
