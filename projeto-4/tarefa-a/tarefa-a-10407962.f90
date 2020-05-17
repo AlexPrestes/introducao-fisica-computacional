c170446acae2528dedf67cea090bb167a7bdeec9
Karen <karen.yonea@hotmail.com>
2020-05-17 01:42:41 -0300

    inicio

--- projeto-4/tarefa-a/tarefa-a-10407962.f90
+++ projeto-4/tarefa-a/tarefa-a-10407962.f90
@@ -1,7 +1,21 @@
 program tarefaa
     implicit real*8 (a-h,o-z)
-    
-    dimension(3) E = (/0.01, 0.001, 0.0001/)
 
-    v(t) = (
+    parameter (pi = 4d0*datan(1d0))
+
+    real(8), dimension(2) :: x, v, a
+
+    x = (/0d0, 100d0/)
+    v0 = 10d0
+    theta = pi/4
+    v = v0*(/dcos(theta), dsin(theta)/)
+    a = (/0d0, -10d0/)
+    h = 0.01
+
+    do while (x(2).ge.0d0)
+        v = v + h*a
+        x = x + h*v
+        print *, x
+    end do
+
 end program tarefaa
