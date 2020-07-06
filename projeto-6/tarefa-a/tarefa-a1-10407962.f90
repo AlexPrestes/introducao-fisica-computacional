bf6e7a0134c11ddbaf9125c42eb0982250f970d9
Alex Prestes <alex.prestes@usp.br>
2020-07-05 23:36:15 -0300

    acabou

--- projeto-6/tarefa-a/tarefa-a1-10407962.f90
+++ projeto-6/tarefa-a/tarefa-a1-10407962.f90
@@ -1,44 +1,39 @@
 program tarefaa1
     implicit real(8) (a-h,o-z)
-    character(70) filename
+    character(70) c_filename
     character(10) c_planeta
     parameter (pi=4d0*atan(1d0))
     dimension ri(2), rmi(2), d2rdt2(2), v(2)
 
-    n = 100
-    Tt = 1d0
-    a_Ms = 2d30
+    n = 5d4
 
-    open(20, file='entrada-a-10407962.dat')
+    open(20, file='entrada-a1-10407962.dat')
 
     do j = 1, 9
-        read(20,*) c_planeta, a_massa, a_raio, a_exce
+        read(20,*) c_planeta, a_massa, a_raio, a_exce, v0
 
-        write(filename, '(A,A,A)') 'saida-a1-', trim(c_planeta), '-10407962.dat'
+        write(c_filename, '(A,A,A)') 'saida-a1-', trim(c_planeta), '-10407962.dat'
 
-        Tp = Tt*sqrt(a_raio**3d0)
+        Tp = sqrt(a_raio**3d0)
         dt = Tp/n
-        GMs = ((2*pi)**2)*(a_raio**3)/(Tp**2)
+        GMs = 4*pi**2
 
-        v0 = 2*pi*a_raio/Tp
-        print *, v0
-        v0 = sqrt(GMs)*sqrt((1/a_raio) * (1 + a_massa/a_Ms))
-        print *, v0
+!        v0 = sqrt(GMs/a_raio)
         v = (/0d0, v0 /)
 
-        write(*,*) c_planeta, dnorm(v), Tp/Tt
-    
-        rmi = (/ a_raio, 0d0 /)
-        ri = rmi +v*dt
+        ri = (/ a_raio, 0d0 /)
+        rmi = ri +v*dt
+        
+        write(*,*) c_planeta, Tp**2/dnorm(ri)**3, v0
 
-        open(10, file=filename)
+        open(10, file=c_filename)
 
         write(10, *) ri
 
-        do i = 1, n+1
+        do i = 1, int(Tp/dt)
             d2rdt2 = -GMs*ri/dnorm(ri)**3
 
-            call verlet(ri, rmi, d2rdt2, dt)
+            call verlet(rmi, ri, d2rdt2, dt)
 
             write(10, *) ri
         end do
@@ -54,14 +49,6 @@ contains
         return
     end function
 
-    function ac_gravit(G, aM, r)
-        implicit real(8) (a-h,o-z)
-        dimension r(2), ac_gravit(2)
-
-        ac_gravit = G*aM*r/dnorm(r)**3
-        return
-    end function
-
     subroutine verlet(ri, rmi, d2rdt2, dt)
         implicit real(8) (a-h,o-z)
         dimension ri(2), rmi(2), raux(2), d2rdt2(2)
@@ -69,7 +56,6 @@ contains
         raux = ri
         ri = 2*ri - rmi + d2rdt2*dt**2
         rmi = raux
-        
     end subroutine
 
 end program tarefaa1
--- projeto-6/tarefa-a/tarefa-a2-10407962.f90
+++ projeto-6/tarefa-a/tarefa-a2-10407962.f90
@@ -1,40 +1,39 @@
 program tarefaa2
     implicit real(8) (a-h,o-z)
-    character(70) filename
+    character(70) c_filename
     character(10) c_planeta
     parameter (pi=4d0*atan(1d0))
     dimension ri(2), rmi(2), d2rdt2(2), v(2)
 
-    dt = 1d-2
-    Tt = 1d0
-    a_Ms = 2d30
+    n = 5d4
 
-    open(20, file='entrada-a-10407962.dat')
+    open(20, file='entrada-a2-10407962.dat')
 
     do j = 1, 9
-        read(20,*) c_planeta, a_massa, a_raio, a_exce
+        read(20,*) c_planeta, a_Mp, a_raio, a_exce, v0
 
-        write(filename, '(A,A,A)') 'saida-a2-', trim(c_planeta), '-10407962.dat'
+        write(c_filename, '(A,A,A)') 'saida-a2-', trim(c_planeta), '-10407962.dat'
 
-        Tp = Tt*sqrt(a_raio**3d0)
-        GMs = ((2*pi)**2)*(a_raio**3)/(Tp**2)
+        Tp = sqrt(a_raio**3d0)
+        dt = Tp/n
+        GMs = 4*pi**2
 
-        v0 = sqrt(GMs)*sqrt((1-a_exce)/(a_raio*(1+a_exce)) * (1 +a_massa/a_Ms))
+!        v0 = sqrt(GMs*(1-a_exce)/(a_raio*(1+a_exce)))
         v = (/0d0, v0 /)
 
-        write(*,*) c_planeta, dnorm(v), Tp/Tt
-    
-        rmi = (/ a_raio, 0d0 /)
-        ri = rmi +v*dt
+        write(*,*) c_planeta, Tp**2/a_raio**3, v0
 
-        open(10, file=filename)
+        ri = (/ a_raio*(1+a_exce), 0d0 /)
+        rmi = ri +v*dt
+
+        open(10, file=c_filename)
 
         write(10, *) ri
 
-        do i = 1, int(Tp/dt)+1
+        do i = 1, n
             d2rdt2 = -GMs*ri/dnorm(ri)**3
 
-            call verlet(ri, rmi, d2rdt2, dt)
+            call verlet(rmi, ri, d2rdt2, dt)
 
             write(10, *) ri
         end do
@@ -50,14 +49,6 @@ contains
         return
     end function
 
-    function ac_gravit(G, aM, r)
-        implicit real(8) (a-h,o-z)
-        dimension r(2), ac_gravit(2)
-
-        ac_gravit = G*aM*r/dnorm(r)**3
-        return
-    end function
-
     subroutine verlet(ri, rmi, d2rdt2, dt)
         implicit real(8) (a-h,o-z)
         dimension ri(2), rmi(2), raux(2), d2rdt2(2)
@@ -65,7 +56,6 @@ contains
         raux = ri
         ri = 2*ri - rmi + d2rdt2*dt**2
         rmi = raux
-        
     end subroutine
 
 end program tarefaa2
